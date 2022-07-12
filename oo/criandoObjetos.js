f2f2b6ecaccad70cc2a0479fd728daab08e7a5c1
Joyce-Ribeiro <58228924+Joyce-Ribeiro@users.noreply.github.com>
2022-07-11 22:09:47 -0300

    aula_11_07_2022

---
+++ oo/criandoObjetos.js
@@ -0,0 +1,45 @@
+//usando a notção literal
+const obj1 = {}
+console.log(obj1)
+
+//Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+//Funções construtoras preco e desc são atributos privados pois não tem o this
+function Produto(nome, preco, desc){
+    this.nome = nome
+    this.getPrecoComDesconto = ()=>{
+        return preco * (1-desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998.99, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+//Função Factory
+function criarFuncionario(nome, salarioBase, faltas){
+    return{
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (this.salarioBase/30)*(30-faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('Joyce', 7980, 4)
+const f2 = criarFuncionario('Samuel', 11400, 1)
+console.log(f1.getSalario(),f2.getSalario())
+
+//Object.create
+const filha = Object.create(null)
+filha.nome= 'Ana'
+console.log(filha)
+
+//Com JSON
+const fromJSON = JSON.parse('{"info":"Sou um JSON"}')
+console.log(fromJSON.info)
