6db3695e7143d7e3a3501f781c838a8e2c9b8a70
Joyce-Ribeiro <58228924+Joyce-Ribeiro@users.noreply.github.com>
2022-09-12 17:41:46 -0300

    aula_12_09_2022

---
+++ ESNext/operadorSpreadRest.js
@@ -0,0 +1,20 @@
+//operador rest como parametro
+function total(...numeros){
+    let total = 0
+    numeros.forEach(n => total += n)
+    return total
+}
+console.log(total(2,3,4,5))
+
+// usar spread com objeto
+const funcionario = { nome: 'Maria', salario:12348.99 }
+const clone = {ativo: true, ...funcionario}
+console.log(clone)
+
+//usar spread com array
+const grupoA =['João','Pedro','Gloria']
+const grupoFinal = ['Maria', ...grupoA,'Rafaela']
+console.log(grupoFinal)
+
+
+//anotar
